6c800bacd8de77f56b9649a3f817caa678307b80
Donald Nguyen <dolnguyen@ucdavis.edu>
2018-05-14 17:06:54 -0700

    added ability to save exercise, sets and reps, and one weight

--- src/index.js
+++ src/index.js
@@ -29,13 +29,13 @@ class WorkoutRows extends React.Component {
         for(let i = 0; i < this.props.numRows; i++){
             grid.push(                
                 <div key={i} className="row">
-                    <div className="col-lg-2 table"><input type="text" name="exercise"/> </div>
-                    <div className="col-lg-2 table"><input className="setAndRep" type="text" name="sets"/>x
-                    <input className="setAndRep" type="text" name="reps"/></div>
-                    <div className="col-lg-2 table"><input type="text" name="weight"/> </div>
-                    <div className="col-lg-2 table"><input type="text" name="totalweight"/> </div>
-                    <div className="col-lg-2 table"><input type="button" value="Yes"/><input type="button" value="No"/></div> 
-                    <div className="col-lg-2 table"><input type="button" onClick={() => this.props.removeRow(i)} value="X" /></div>
+                    <div className="col-lg-2 table"><input onChange={(event) => this.props.handleChange("exercise",i,event)} type="text" name="exercise"/> </div>
+                    <div className="col-lg-2 table"><input onChange={(event) => this.props.handleChange("sets",i,event)} className="setAndRep" type="number" name="sets"/>x
+                    <input onChange={(event) => this.props.handleChange("reps",i,event)} className="setAndRep" type="number" name="reps"/></div>
+                    <div className="col-lg-2 table"><input onChange={(event) => this.props.handleChange("weight",i,event)} type="number" name="weight"/> </div>
+                    <div className="col-lg-2 table"><input type="number" name="totalweight"/> </div>
+                    <div className="col-lg-2 table"><input type="checkbox"/></div> 
+                    <div className="col-lg-2 table"><input type="button" onClick={() => this.props.removeRow(i)} value="X" /></div>                    
                 </div>
             );
         }
@@ -72,14 +72,27 @@ class WorkoutBox extends React.Component {
 
             }], // key is the name of the exercise, value is the object rowElements
             numDays: 0,              // integer to keep track of the number of day tabs to render
-            numRows: 0,
+            numRows: 1,
         };
+        // bind the context of this to the workout box object
         this.addNewRow = this.addNewRow.bind(this);
         this.removeRow = this.removeRow.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
     }
+    handleChange(field,saveIndex,event){
+        var saveField = this.state.exerciseRows.slice();
+        saveField[saveIndex][field] = event.target.value;
+
+        this.setState({
+            exerciseRows: saveField,
+        });
+        console.log(this.state.exerciseRows);
+    }
+
     addNewRow(currRows){
         // capping the number of rows to 10
-        var exerciseRows = this.state.exerciseRows;
+        var exerciseRows = this.state.exerciseRows.slice();
         if(currRows === 10){
             return;
         }
@@ -96,26 +109,31 @@ class WorkoutBox extends React.Component {
         }
     }
     removeRow(index){
-        console.log(index);
         var exerciseRows1 = this.state.exerciseRows.slice(0,index);
-        var exerciseRows2 = this.state.exerciseRows.slice(index+1);
-        var newexRow = exerciseRows1.push(exerciseRows2);
+        var exerciseRows2 = this.state.exerciseRows.slice(index+1, this.state.exerciseRows.length);
+
+        // slice out the removed row and put the other rows back together
+        for(let i = 0; i < exerciseRows2.length; i++){
+            exerciseRows1.push(exerciseRows2[i]);
+        }
+
         var newRows = this.state.numRows;
         this.setState({ 
-            exerciseRows: newexRow,
+            exerciseRows: exerciseRows1,
             numRows: newRows-1, 
         });
     }
     render(){
-        // <workoutRows />
-
         return (
-        <div className="container">
-            <DaysOfWeek />
-            <WorkoutRows numRows={this.state.numRows} removeRow={this.removeRow} addNewRow={() => this.addNewRow(this.state.numRows)}/>
-        </div>
+            <div className="container">
+                <DaysOfWeek />
+                <WorkoutRows onChange={this.handleChange} numRows={this.state.numRows} removeRow={this.removeRow} 
+                addNewRow={() => this.addNewRow(this.state.numRows)}
+                handleChange={this.handleChange}/>
+            </div>
+        )
+    }
 
-        )}
 }
 
 ReactDOM.render(<WorkoutBox />, document.getElementById("root"));
